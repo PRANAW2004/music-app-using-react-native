049af5949f13c47059fa5026e8ff4c31cf6b8669
PRANAW2004 <pranawrk2@gmail.com>
2024-01-31 16:59:13 +0530

    Updated App

--- components/screens/folk.jsx
+++ components/screens/folk.jsx
@@ -1,4 +1,4 @@
-import { View, Text,StyleSheet,BackHandler,ScrollView,Image } from 'react-native';
+import { View, Text,StyleSheet,BackHandler,ScrollView,Image, Pressable } from 'react-native';
 import { useEffect,useState } from 'react';
 import data from '../song_data';
 import Song_Render from '../song-render';
@@ -27,53 +27,7 @@ export default function Folk({navigation}){
         })
     },[])
 
-    //  const setUpTrackPlayer = async () => {
-    //     try{
-    //     await TrackPlayer.setupPlayer()
-    //     TrackPlayer.updateOptions({
-    //         stopWithApp: true,
-    //         capabilities: [
-    //             Capability.Play,
-    //             Capability.Pause,
-    //             Capability.SkipToNext,
-    //             Capability.SkipToPrevious,
-    //         ],
-    //         // compactCapabilities: [
-    //         //     Capability.Play,
-    //         //     Capability.Pause,
-    //         //     Capability.SkipToNext,
-    //         //     Capability.SkipToPrevious,
-    //         // ]
-    //     })
-
-    //     TrackPlayer.addEventListener("remote-play", () => {
-    //         TrackPlayer.play();
-    //     })
-    //     TrackPlayer.addEventListener("remote-pause", () => {
-    //         TrackPlayer.pause();
-    //     })
-    //          TrackPlayer.addEventListener("remote-next", () => {
-    //         TrackPlayer.skipToNext();
-    //     })
-    //      TrackPlayer.addEventListener("remote-previous", () => {
-    //         TrackPlayer.skipToPrevious();
-    //     })
-    
-    //           await TrackPlayer.add([data]);
-
-    //     TrackPlayer.getProgress().then((e) => setDuration(e));
-
-    //     }
-    //     catch(err){
-    //         console.log(err);
-    //     }
-    // }    
-
-    // useEffect(() => {
-    //     setUpTrackPlayer();
-
-    //     return () => TrackPlayer.destroy();
-    // }, [])
+   
     return(
         <View style={styles.folkview}>
             {/* {data.map((e)=>{
@@ -88,9 +42,8 @@ export default function Folk({navigation}){
 
             })} */}
             <ScrollView style={{width:'100%'}}>
-                <Song_Render data={data}/>
+            <Song_Render data={data}/>
             </ScrollView>
-            <Image source={require("C:\Users\TOSHIBA\Pictures\kitten.png")} />
             </View>
     )
 }
--- components/song-render.jsx
+++ components/song-render.jsx
@@ -1,19 +1,111 @@
-import {View,Text,StyleSheet,Image} from 'react-native';
+import {View,Text,StyleSheet,Image,Pressable} from 'react-native';
+import { useState,useEffect } from 'react';
+import TrackPlayer,{useProgress,Capability, AppKilledPlaybackBehavior,Event} from 'react-native-track-player';
+import data from './song_data';
 
 export default function Song_Render(props){
+
+    const [duration,setDuration] = useState(0);
+    const [bool,setbool] = useState(false);
+
+    const setUpTrackPlayer = async () => {
+        try{
+        await TrackPlayer.setupPlayer()
+        TrackPlayer.updateOptions({
+            stopWithApp: true,
+            capabilities: [
+                Capability.Play,
+                Capability.Pause,
+                Capability.SkipToNext,
+                Capability.SkipToPrevious,
+            ],
+            // compactCapabilities: [
+            //     Capability.Play,
+            //     Capability.Pause,
+            //     Capability.SkipToNext,
+            //     Capability.SkipToPrevious,
+            // ]
+        })
+
+        TrackPlayer.addEventListener("remote-play", () => {
+            TrackPlayer.play();
+        })
+        TrackPlayer.addEventListener("remote-pause", () => {
+            TrackPlayer.pause();
+        })
+             TrackPlayer.addEventListener("remote-next", () => {
+            TrackPlayer.skipToNext();
+        })
+         TrackPlayer.addEventListener("remote-previous", () => {
+            TrackPlayer.skipToPrevious();
+        })
+
+        }
+        catch(err){
+            console.log(err);
+        }
+    }    
+
+    useEffect(() => {
+        setUpTrackPlayer();
+
+        return () => TrackPlayer.destroy();
+    }, [])
+
+    async function play(id){
+        // console.log(data);
+        await TrackPlayer.reset(); 
+        if(bool === false){
+            console.log(bool);
+            for(var i=0;i<data.length;i++){
+                if(data[i]['id'] === id){
+                    // console.log(data[i]);
+                    TrackPlayer.add([data[i]]);
+                    // console.log(await TrackPlayer.add([data[i]]));
+                    TrackPlayer.play();
+                    setbool(true);
+                    break;
+                }
+            }
+        }else{
+            console.log(bool);
+            // TrackPlayer.pause();
+            for(var i=0;i<data.length;i++){
+                if(data[i]['id'] === id){
+                    // console.log(data[i]);
+                    TrackPlayer.add([data[i]]);
+                    TrackPlayer.play();
+                    setbool(false);
+                    break;
+                }
+            }
+        }
+        
+        
+        // TrackPlayer.reset();
+    }
+
     return(
         <View style={{width:'100%'}}>
             {/* <Text style={{color: "white"}}>{props.data}</Text> */}
             {props.data.map((e)=>{
-                console.log("'"+e['artwork']+"'");
-                return(            
+                return(
+            <Pressable onPress={()=>{TrackPlayer.pause();play(e['id'])}}>
+                    {/* {console.log(TrackPlayer.getProgress().then((e) => console.log(e)))}
+                    {console.log(TrackPlayer.play())} */}
                     <View style={{width:'100%',display:"flex",alignItems: "center"}}>
                     <View style={styles.songblock}>
-                    <Image source={{uri: "'"+e['artwork']+"'"}} style={{height: 40,width:40}}/>
-                    <Text style={{color: "black"}}>{e['id']}</Text>
+                    <View style={{marginRight: 10}}>
+                    <Image source={{uri: e['artwork']}} style={{height: 60,width:60,borderRadius:36}}/>
+                    </View> 
+                    <View>
+                    <Text style={{color: "white",fontSize: 20}}>{e['title']}</Text>
+                    <Text style={{color: "white"}}>{e['artist']}</Text>
+                    </View>
 
                     </View>
                     </View> 
+                    </Pressable>
                 )
 
             })}
@@ -31,15 +123,15 @@ const styles = StyleSheet.create({
     },
     songblock: {
         display: "flex",
-        backgroundColor: "lightblue",
+        backgroundColor: "#492E87",
         // marginBottom: 10,
         // padding: 10,
         width: '90%',
         marginTop: 20,
-        borderRadius: 20,
+        borderRadius: 30,
         flexDirection: "row",
-        // alignItems: "center",
-        justifyContent: "center",
+        alignItems: "center",
+        // justifyContent: "center",
         height: 60,
         // height: '100%',
     }
