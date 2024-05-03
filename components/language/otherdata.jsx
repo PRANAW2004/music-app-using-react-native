f0518b276163a2bafee162b55ad74b6e74fb8d26
PRANAW2004 <pranawrk2@gmail.com>
2024-05-04 00:11:20 +0530

    initial commit

---
+++ components/language/otherdata.jsx
@@ -0,0 +1,276 @@
+const otherdata = [
+    {
+        id:1,
+        title: "Acharuli Gandagana [ TRAP REMIX]",
+        artist: "Georgian Folk",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FAcharuli%20Gandagana%20%5B%20TRAP%20REMIX%5D.mp3?alt=media&token=a35153d7-c40d-4dff-8487-da1868b08e4a",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fachrauli.jpg?alt=media&token=621683ec-3594-4ea7-a755-10747c1f6c03",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:2,
+        title: "O Fortuna",
+        artist: "Carl Orff",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FAndr%C3%A9%20Rieu%20-%20O%20Fortuna.mp3?alt=media&token=f47e0e5b-f0f0-4250-ba6f-77efea573734",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Ffortuna.webp?alt=media&token=410312fc-b29a-490e-9639-57677c988916",
+        liked: "cards-heart-outline",
+        color: "white"
+
+    },
+    {
+        id:3,
+        title: "Bre Petrunko",
+        artist: "Koutev Bulgarian National Ensemble",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FBre%20Petrunko.mp3?alt=media&token=2ce11d3c-b047-42b9-ba3f-b3ffedd2fe17",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fpertunko.jpg?alt=media&token=a67a9897-81fc-4eee-8554-59371c0e2129",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:4,
+        title: "Cleopatra Stratan - Zunea Zunea",
+        artist: "Cleopatra Stratan",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FCleopatra%20Stratan%20-%20Zunea%20Zunea.mp3?alt=media&token=25a63070-7da8-46c3-b077-4a3921adabfa",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fzunea.jpg?alt=media&token=025073b6-2c3e-4f8c-8d37-f2bc8331af9d",
+        liked: "cards-heart-outline",
+        color: "white" 
+    },
+    {
+        id:5,
+        title: "Gasolina",
+        artist: "Daddy Yankee",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FDaddy%20Yankee%20-%20Gasolina.mp3?alt=media&token=a42eaec6-6dac-477a-a7bc-4da6e76f4e1d",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fgasolina.jpg?alt=media&token=b4c1de0e-a4c8-4f45-8046-0ae6f94799fb",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:6,
+        title: "Tokyo Drift",
+        artist: "Teriyaki Boyz",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FDj%20Kantik%20-%20Teriyaki%20Boyz.mp3?alt=media&token=86a926de-6119-425a-84d6-09de188f9812",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Ftokyo.jpg?alt=media&token=03684e3d-f4e3-4a40-8113-2dd8e7c95b87",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:7,
+        title: "Bailando",
+        artist: "Enrique Iglesias",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FEnrique%20Iglesias%20-%20Bailando.mp3?alt=media&token=dc4e836f-42e8-4004-b8f9-0f2734149d83",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fbailando.jpg?alt=media&token=a3a757ef-5a81-4d90-be5b-f84cf85ce7f7",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:8,
+        title: "SUBEME LA RADIO",
+        artist: "Enrique Iglesias",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FEnrique%20Iglesias%20-%20SUBEME%20LA%20RADIO.mp3?alt=media&token=ab1d35ed-2c0b-4f58-86fe-d03bd147e1d8",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fsubema.jpg?alt=media&token=75cef865-709d-41b5-a691-e39414cceb5d",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:9,
+        title: "Pepas",
+        artist: "Farruko",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FFarruko%20-%20Pepas.mp3?alt=media&token=f1e3c31c-e6da-4dbf-92bb-0b4763026339",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fpepas.webp?alt=media&token=8f0d4c72-531d-4205-bb41-1b889c8f2376",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:10,
+        title: "Gangnam Style",
+        artist: "PSY",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FGangnam%20Style%20PSY.mp3?alt=media&token=4027af36-c0e2-4582-bebc-121a26c44a1d",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fgangam.png?alt=media&token=3aaa4f76-8d5d-464a-a1ee-692228292efd",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:11,
+        title: "Habibi",
+        artist: "Dardan,zuna,Ricky Rick",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FHabibi.mp3?alt=media&token=dfc4fb19-b69d-499e-b05d-aa638569ab67",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fhabibi.png?alt=media&token=1d31b250-beb4-48e3-a0a0-fa86aa18ebe4",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:12,
+        title: "I'm an Albatraoz",
+        artist: "AronChupa",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FI'm%20an%20Albatraoz.mp3?alt=media&token=ac7ae575-40ce-44e4-bb26-3335110b9d12",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Falbatroz.jpg?alt=media&token=a7dc93d4-d670-45d2-a03c-62e2c34a9204",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:13,
+        title: "Satisfya",
+        artist: "Imran Khan",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FImran%20Khan%20-%20Satisfya.mp3?alt=media&token=db52636e-3858-4dae-8bc2-c8b23c79ddfa",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fsatisfya.jpeg?alt=media&token=7282d3a1-79c0-4c62-9757-953873fd9386",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:14,
+        title: "Ainsi Bas La Vida",
+        artist: "Indilia",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FIndila%20-%20Ainsi%20Bas%20La%20Vida.mp3?alt=media&token=d185662c-5d85-4d0c-95b7-918c6ffdf0bf",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fainsi.jpeg?alt=media&token=413b95b5-4593-4299-a03c-52a159c69e94",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:15,
+        title: "Dernière Danse",
+        artist: "Indilia",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FIndila%20-%20Derni%C3%A8re%20Danse.mp3?alt=media&token=fbc3f3db-3d8e-4a16-9222-d59c1d409681",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Findilia.png?alt=media&token=471a97f2-8cca-4ffc-bb68-09e2897a3dcc",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:16,
+        title: "Lambada",
+        artist: "Kaoma",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FKaoma%20-%20Lambada.mp3?alt=media&token=f720b2c1-a083-4f6a-8f00-65c8345c19d2",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Flambada.jpg?alt=media&token=1fc87cd7-0e75-4f87-8a56-5b31ff74452c",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:17,
+        title: "C'est La Vie",
+        artist: "Khaled",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FKhaled%20-%20C'est%20La%20Vie.mp3?alt=media&token=9ed16e44-40f1-44ee-8fea-2fc614277c62",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fkhaled.jpg?alt=media&token=0fd52f5b-2bef-4f45-9ef9-215c01f916d1",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:18,
+        title: "Bella Ciao",
+        artist: "El Profesor",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FLa%20Casa%20De%20Papel%20-%20Bella%20Ciao.mp3?alt=media&token=89eb390b-b866-4530-bddc-b5bb1e321129",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fbellaciao.jpg?alt=media&token=eaeeba13-af06-4c2d-97a1-3215ecff321c",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:19,
+        title: "Kosandra",
+        artist: "Miyagi & Andy Panda",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FMiyagi%20%26%20Andy%20-%20Kosandra.mp3?alt=media&token=c508709d-c333-4904-ac6d-3f1b7cd857ba",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fkosandra.jpg?alt=media&token=f5abcc2f-ef4d-4f41-958c-ec96b337ef45",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:20,
+        title: "Pembe Mezarlık",
+        artist: "Model",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FModel%20-%20Pembe%20Mezarl%C4%B1k.mp3?alt=media&token=9cfc95db-52e8-4780-b72f-6463fef29704",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fmodel.png?alt=media&token=445e4390-d60e-478b-ae9a-ff498c436e51",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:21,
+        title: "Liyakun Yawmuka",
+        artist: "Nasheed",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FNasheed%20-%20Liyakun%20Yawmuka.mp3?alt=media&token=5c109fca-26ce-4a9f-9811-5c9d46f78586",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fnasheed.jpg?alt=media&token=da0057fc-daa9-48a2-a368-bf239505fddf",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:22,
+        title: "детство",
+        artist: "rauf and faik",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FRauf%20%26%20Faik%20-%20%D0%B4%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%BE.mp3?alt=media&token=f30a8fe3-87af-4b35-a29b-8b5638e887ac",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Frauf.jpeg?alt=media&token=2ff12bee-4114-46c6-b530-a8092031c43a",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:23,
+        title: "колыбельная",
+        artist: "rauf and faik",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FRauf%20%26%20Faik%20-%20%D0%BA%D0%BE%D0%BB%D1%8B%D0%B1%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F.mp3?alt=media&token=330a236d-06a2-4f7a-b4e6-48e5037f378a",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Ffaik.jpg?alt=media&token=f39fb81d-945f-44dc-9e28-28266dfe5f19",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:24,
+        title: "Mujeriego",
+        artist: "Ryan Castro",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FRyan%20Castro%20-%20Mujeriego.mp3?alt=media&token=d98d66ed-2a90-49f5-a691-bd0c1873ce25",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fryan.jpg?alt=media&token=ae8d0dac-e025-4724-85e4-8200daffc908",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:25,
+        title: "Hislerim",
+        artist: "Serhat Durmus and Zerrin",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FSerhat%20Durmus%20-%20Hislerim%20(ft.%20Zerrin).mp3?alt=media&token=9f762937-c917-48cf-9dea-50377404037e",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fhislerim.jpeg?alt=media&token=be02d98e-a9c3-40a4-931c-68af4daa7424",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:26,
+        title: "Una Noche",
+        artist: "souf",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FSouf%20-%20Una%20Noche.mp3?alt=media&token=aca838d7-0fe6-4773-8fc5-8edd8c5708b3",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fsouf.jpeg?alt=media&token=9558b178-ed1f-4148-b38a-c8912eb76e65",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:27,
+        title: "papaoutai",
+        artist: "Stromae",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FStromae%20-%20papaoutai.mp3?alt=media&token=b1825528-f54b-462f-9acb-012dfe49e482",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fstrromae.jpg?alt=media&token=22729c18-8a5e-42ae-b46b-2bfe9aa0170d",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:28,
+        title: "Taki Taki",
+        artist: "DJ Snake",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FTaki%20Taki.mp3?alt=media&token=7eeea429-b493-4a2d-a7fc-4e9355f1c08a",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Ftaki.jpg?alt=media&token=9f0f276f-f8f7-4937-8ee2-1cc5dfaa9f98",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:29,
+        title: "Dinero",
+        artist: "Trinidad Cardona",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2FTrinidad%20Cardona%20-%20Dinero.mp3?alt=media&token=1822c45d-8167-4921-9a77-2c67bf17504f",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fdinero.jpeg?alt=media&token=10770daf-7911-4ef8-8abf-e9798abbe7ee",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:30,
+        title: "Despacito",
+        artist: "Luis Fonsi",
+        url: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/songs%2Fother%2Fdespacito.mp3?alt=media&token=5c8c5d68-098d-44de-8463-ad5d866ea724",
+        artwork: "https://firebasestorage.googleapis.com/v0/b/musicapp-c920a.appspot.com/o/images%2Fother%2Fdespacito.png?alt=media&token=9745ac84-f383-4e92-909e-4b4fe5215e81",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    
+]
+
+export default otherdata;
