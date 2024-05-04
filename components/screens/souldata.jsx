dd94d9822d7bed385ce0ebaf42680e01458a3115
PRANAW2004 <pranawrk2@gmail.com>
2024-05-04 23:43:08 +0530

    Updated App

---
+++ components/screens/souldata.jsx
@@ -0,0 +1,456 @@
+const souldata = [
+    {
+        id:1,
+        title: "After All",
+        artist: "Noha Saré",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:2,
+        title: "All My Love",
+        artist: "KIRBY",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+
+    },
+    {
+        id:3,
+        title: "Back to Me",
+        artist: "Moonchild",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:4,
+        title: "Biting These Bullets",
+        artist: "NAVENI",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white" 
+    },
+    {
+        id:5,
+        title: "Can't Have It All",
+        artist: "Cobi",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:6,
+        title: "Coffee in the Morning",
+        artist: "Aaron Taylor ",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:7,
+        title: "Coolin",
+        artist: "Jacob Banks",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:8,
+        title: "Cure",
+        artist: "Moonchild",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:9,
+        title: "Deliverer",
+        artist: "Antoine Bradford",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:10,
+        title: "Don't Lay It All On Me",
+        artist: "Janice",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:11,
+        title: "GTC",
+        artist: "Kubla",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:12,
+        title: "Good Man Down",
+        artist: "Common Hymnal",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:13,
+        title: "Good Things Fall Apart",
+        artist: "Rhonda",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:14,
+        title: "Hiding",
+        artist: "JELISA",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:15,
+        title: "Hills",
+        artist: "Naomi Sharon",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:16,
+        title: "Horsepower For The Streets",
+        artist: "Jonathan Jeremiah",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:17,
+        title: "Human being",
+        artist: "Cézanne",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:18,
+        title: "I'm Different",
+        artist: "Debbie",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:19,
+        title: "Is It Any Wonder? ",
+        artist: "Durand Jones & The Indications",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:20,
+        title: "Is This Love",
+        artist: "Allen Stone",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:21,
+        title: "I’m Leaving",
+        artist: "The Teskey Brothers",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:22,
+        title: "Kaleidoscope",
+        artist: "Otis Kane",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:23,
+        title: "Let me try",
+        artist: "Jamily",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:24,
+        title: "Lonely Love Song",
+        artist: "St. Paul & The Broken Bones",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:25,
+        title: "Lose Control",
+        artist: "Teddy Swims",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:26,
+        title: "Love Is A Losing Game",
+        artist: "TEEKS",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:27,
+        title: "Lover, You Should’ve Come Over",
+        artist: "Elmiene",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:28,
+        title: "Mad About You",
+        artist: "Son Little",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:29,
+        title: "Magic",
+        artist: "Sabrina Starke",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:30,
+        title: "No Clouds No Love",
+        artist: "whothefislou",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:31,
+        title: "Not Serious",
+        artist: "Mychelle",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:32,
+        title: "Rent Free",
+        artist: "6LACK",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+
+    },
+    {
+        id:33,
+        title: "SOMEWHERE FAR",
+        artist: "Jordan Astra",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:34,
+        title: "STILL HAVE YOU",
+        artist: "Jake Isaac",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white" 
+    },
+    {
+        id:35,
+        title: "Say Something",
+        artist: "Sinéad Harnett",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:36,
+        title: "Self Sabotage",
+        artist: "Mychelle",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:37,
+        title: "Someday-Unplugged",
+        artist: "Elmiene",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:38,
+        title: "Southern Comfort",
+        artist: "Bluey Robinson",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:39,
+        title: "The Truth",
+        artist: "Moonchild",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:40,
+        title: "Things Meant For Me",
+        artist: "Eric Roberson",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:41,
+        title: "This Life",
+        artist: "Kendra Morris",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:42,
+        title: "Through The Night",
+        artist: "Maeta",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:43,
+        title: "To be naive",
+        artist: "Adja",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:44,
+        title: "Unaware",
+        artist: "Allen Stone",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:45,
+        title: "Video Game",
+        artist: "Budjerah",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:46,
+        title: "Wake Up",
+        artist: "Majo",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:47,
+        title: "Where Do We Go",
+        artist: "Andra Day",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:48,
+        title: "Whoever You Are",
+        artist: "ELIZA",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:49,
+        title: "burn out",
+        artist: "SOLOMON",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    },
+    {
+        id:50,
+        title: "i would if i could",
+        artist: "Santino Le Saint",
+        url: "",
+        artwork: "",
+        liked: "cards-heart-outline",
+        color: "white"
+    }
+]
+
+export default souldata;
